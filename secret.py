b2363851ea4037207aadacf2083b96d3f8c48f6c
ABI-DEV-BIO <abimendoza835@gmail.com>
2026-07-03 09:53:33 +0500

    Crear secret.py
    secreto...

---
+++ secret.py
@@ -0,0 +1,6 @@
+mi_secreto = "Me gusta programar"
+respuesta = input("¿Cual es el secreto?")
+if respuesta==mi_secreto:
+    print("¡Tienes razon si es mi secreto!")
+else:
+    print("Oh lo siento te equivocaste")
