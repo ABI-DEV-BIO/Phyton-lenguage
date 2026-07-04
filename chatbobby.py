51a725340e693c04a383878cad5181d9b3e23091
ABI-DEV-BIO <abimendoza835@gmail.com>
2026-07-04 08:30:44 +0500

    Crear chatbobby.py v1 chat bot

---
+++ chatbobby.py
@@ -0,0 +1,21 @@
+print("BOBBY 1.0")
+
+#1.Saludo
+print("BOT: Hola como te llamas bro?.")
+user_input=input()
+
+print(f"BOBBY:Que buen nombre {user_input}")
+print(f"BOBBY:Y que tal {user_input} como te va?")
+user_input=input()
+
+print("BOBBY:!Super abi me alegro por ti¡")
+print("BOBBY:!Cuentame bro cuantos años tienes?")
+user_input=input()
+
+print(f"BOBBY:Esta genial que tengas {user_input} y que ya tengas la mente en la programacion!")
+print("BOBBY:Y que sueños tienes aparte de programar hermano?")
+user_input=input()
+
+print(f"{user_input}?! Eso es todo hermano es genial que tengas esos sueños!.")
+user_input=input()
+print("BOBBY:Broo!! no sabia que te gustaba dibujar!!🙌🏻🙌🏻😌")
