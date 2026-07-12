8fe268fa86e3fff917dc6e3f8eb203f9eee65cf4
ABI-DEV-BIO <abimendoza835@gmail.com>
2026-07-12 10:56:07 +0500

    Crear yummy.py

---
+++ yummy.py
@@ -0,0 +1,13 @@
+comida = input("¿What do you want to eat? ").lower()
+
+if comida == "pizza":
+    print("Calling Dominos: 33618392🍕")
+    
+elif comida == "burger":
+    print("Calling Burger King: 6191838🍔")
+    
+elif comida == "tacos":
+    print("Calling Tacos Chipotle: 82929301🌮")
+    
+else:
+    exit()
